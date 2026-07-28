61c3e6788c29819d8376e5d00e30aeebab3e0938
Galdino <galdi1230@gmail.com>
2026-07-27 20:33:04 -0600

    Entorno virtual correcciones

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-echo "Activando el entorno virtual"
-if [!"-d venv"]; then
+echo "Iniciando ejecucion de pruebas Jenkins..."
+
+#Verificar si el entorno virtual existe
+if [ ! -d "venv" ]; then
+    echo "Entorno virtual no encontrado. Creandolo..."
     python3 -m venv venv
 fi
-source venv/bin/activate
 
-echo "Instalando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+#Activar el entorno virtual correctamente
+if [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+elif [ -f "venv/Scripts/activate" ]; then #Para windows
+    source venv/Scripts/activate
+else
+    echo "Error: No se pudo activar el entorno virtual"
+    exit 1
+fi
 
-echo "Ejecutando pruebas con pytest"
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+#Verificar si pip esta instalado correctamente
+echo "Instalando dependencias..."
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
+
+#Ejecutar las pruebas
+echo "Ejecutando pruebas con pytest..."
+venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/report.html --self-contained-html
 
 echo "Pruebas finalizadas resultados en reports"
